52003ac7f2fb1cae86f7781dd41f94ce0bc686d5
EGO <emiliegoudeau.eg@gmail.com>
2017-05-19 17:04:06 +0200

    Derniere modif 19/05/17 17h04 EGO

--- SuzanneLea/src/main/java/service/IAidantService.java
+++ SuzanneLea/src/main/java/service/IAidantService.java
@@ -1,5 +1,11 @@
 package service;
 
+import java.util.List;
+
+import bean.Personne;
+
 public interface IAidantService {
+	
+	public List<Personne> getListPersonne() ;
 
 }
--- SuzanneLea/src/main/java/servlet/Login.java
+++ SuzanneLea/src/main/java/servlet/Login.java
@@ -5,6 +5,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Personne;
+import service.AidantService;
 
 
 //@WebServlet("/Login")
@@ -18,16 +22,28 @@ public class Login extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		AidantService.getInstance().getListPersonne();
+		this.getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward (request, response);
 		
-		//this.getServletContext().getRequestDispatcher("/dashboard").forward (request, response);
 		
+		
+	
 		//Pour rediriger vers la servlet Dashboard via l'url dashboard
-		response.sendRedirect("dashboard");
+		//response.sendRedirect("dashboard");
 	}
 
 	
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String redirection = "";
+		String mail = request.getParameter("mail");
+		String mdp = request.getParameter("mdp");
+		
+		HttpSession session = request.getSession();
+		
+		
+		
+		
 		
 		doGet(request, response);
 	}
